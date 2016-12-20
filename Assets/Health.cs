18e1b7382f0376d0658e55727f221d3f1d6e70b7
khoand <khoas401@gmail.com>
2016-12-20 20:48:50 +0700

    sửa scence (reverted from commit a754c2232f04bf497238e088b1251c53056048a7)

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -1,19 +1,45 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
 
-public class Health : MonoBehaviour {
-    public RectTransform healthBar;
+public class Health : NetworkBehaviour
+{
+
     public const int maxHealth = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = maxHealth;
+
+    public RectTransform healthBar;
+
     public void TakeDamage(int amount)
     {
+        if (!isServer)
+            return;
+
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
-            currentHealth = 0;
-            Debug.Log("Dead!");
+            currentHealth = maxHealth;
+
+            // called on the Server, but invoked on the Clients
+            RpcRespawn();
         }
+    }
+
+    void OnChangeHealth(int currentHealth)
+    {
         healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+        {
+            // move back to zero location
+            transform.position = Vector3.zero;
+        }
     }
 }
